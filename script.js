8ab4c56f4a1428de98dceefc394bfd147bdee920
mansaa0n <mansaa0n@gmail.com>
2021-05-17 22:55:59 +0000

    changes on all files

--- script.js
+++ script.js
@@ -5,11 +5,31 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+    if (principal <= 0){
+        alert("Enter a positive number")
+    }else{
+        var results = "If you deposit " + principal 
+                    + "\n" 
+                    + "at an interest rate of " + rate
+                    + "\n"
+                    + "You will receive an amount of " + interest
+                    + "\n"
+                    + "in the year " + year 
+        document.getElementById("result").innerText = results
+    }
+
 }
         
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-
-    document.getElementById("rate_val").innerText="rateval";
+    var color = 'linear-gradient(90deg, red' + 50 + '%, white 10%, gray)';
+    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate").style.background = 'linear-gradient(90deg, blue '+rateval*4.75+'%, #C1C1C1 '+(rateval*4.75)+'%)';
 }
+// function slider_lower_bound_color(){
+//     var slider = document.getElementById("rate");
+//     var color = 'linear-gradient(90deg, red' + slider.value + '%, white 10%, gray)';
+//     slider.style.background = color;
+//     // linear-gradient(0deg, blue, green 40%, red);
+// }
